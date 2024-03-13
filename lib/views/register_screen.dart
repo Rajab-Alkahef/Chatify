caa9aed8826a4b1ed6508e3026ddcdf49713d842
Rajab-Alkahef <rajabalkahef3@gmail.com>
2024-03-13 12:24:43 +0300

    checking password before creating an account and update the username

--- lib/views/register_screen.dart
+++ lib/views/register_screen.dart
@@ -9,6 +9,8 @@ class RegisterScreen extends StatelessWidget {
   static String id = 'register_screen';
   String? email;
   String? password;
+  String? username;
+  String? confirmpassword;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,6 +41,9 @@ class RegisterScreen extends StatelessWidget {
                 ),
               ),
               customTextField(
+                onChanged: (data) {
+                  username = data;
+                },
                 hintText: 'Username',
               ),
               customTextField(
@@ -54,26 +59,72 @@ class RegisterScreen extends StatelessWidget {
                 hintText: 'Password',
               ),
               customTextField(
+                onChanged: (data) {
+                  confirmpassword = data;
+                },
                 hintText: 'Confirm Password',
               ),
               CustomButton(
                 onTap: () async {
-                  try {
-                    final credential = await FirebaseAuth.instance
-                        .createUserWithEmailAndPassword(
-                      email: email!,
-                      password: password!,
-                    );
-                    print('-------------');
-                    print(credential.user!.displayName);
-                  } on FirebaseAuthException catch (e) {
-                    if (e.code == 'weak-password') {
-                      print('The password provided is too weak.');
-                    } else if (e.code == 'email-already-in-use') {
-                      print('The account already exists for that email.');
+                  // try {
+                  //   final credential = await FirebaseAuth.instance
+                  //       .createUserWithEmailAndPassword(
+                  //     email: email!,
+                  //     password: password!,
+                  //   );
+
+                  //   print('-------------');
+                  //   print(credential.user!.displayName);
+                  // } on FirebaseAuthException catch (e) {
+                  //   if (e.code == 'weak-password') {
+                  //     print('The password provided is too weak.');
+                  //   } else if (e.code == 'email-already-in-use') {
+                  //     print('The account already exists for that email.');
+                  //   }
+                  // } catch (e) {
+                  //   print(e);
+                  // }
+                  // Assuming 'username', 'email', 'password', and 'confirmPassword' are already defined
+                  if (password == confirmpassword) {
+                    try {
+                      final credential = await FirebaseAuth.instance
+                          .createUserWithEmailAndPassword(
+                        email: email!,
+                        password: password!,
+                      );
+                      print(username);
+
+                      await credential.user?.updateDisplayName("Rajab");
+                      await credential.user?.reload();
+                      print('-------------');
+                      var currentUser = FirebaseAuth.instance.currentUser;
+                      print(currentUser?.displayName);
+                      // Navigator.push(
+                      //   context,
+                      //   MaterialPageRoute(
+                      //     builder: (context) {
+                      //       return Scaffold(
+                      //         body: Column(
+                      //           children: [
+                      //             Text(credential.user!.displayName!),
+                      //             Text(username!),
+                      //           ],
+                      //         ),
+                      //       );
+                      //     },
+                      //   ),
+                      // );
+                    } on FirebaseAuthException catch (e) {
+                      if (e.code == 'weak-password') {
+                        print('The password provided is too weak.');
+                      } else if (e.code == 'email-already-in-use') {
+                        print('The account already exists for that email.');
+                      }
+                    } catch (e) {
+                      print(e);
                     }
-                  } catch (e) {
-                    print(e);
+                  } else {
+                    print('The passwords do not match.');
                   }
                 },
                 color: kSecondaryColor,
